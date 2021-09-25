320327e49b1d25ec0fb7dbf538677493e116f5be
a.hoettgen <a.hoettgen@neogramm.de>
2021-09-25 12:28:36 +0200

    Added function to receive data from InfluxDB Added function to send data to InfluxDB Added main function to process data and run anomaly detection

--- anomaly_script.py
+++ anomaly_script.py
@@ -3,16 +3,55 @@ import matplotlib.pyplot as plt
 import numpy as np
 import warnings
 from sklearn.ensemble import IsolationForest
+
+from datetime import datetime
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
 warnings.filterwarnings("ignore")
 
-df = pd.read_csv('/Users/aarsh/Downloads/rssi.csv',usecols=['DateTime','A2_RSSI'],parse_dates=['DateTime'])
-#getting the data(1000 points)
+token = "e-za4dDVDrQAe8g378qovfTvuLZYHZ_VwTwihauVZlrFj6tlVh9QreqmUH5q2_wxqO1k_vjfzR7-vvoNXeN5vA=="
+org = "HackZurich"
+bucket = "hackzurich"
+client = InfluxDBClient(url="http://localhost:8086", token=token)
+
+measurement = 'a2_rssi'
+field = 'signalStrength'
+
+def getDataPoints(count):
+    query = f'from(bucket: "{bucket}") |> range(start: -{count}s) |> filter(fn: (r) => r._measurement == "{measurement}" and r._field == "{field}")'
+    data = { 'timestamp': [], field: [] }
+
+    for record in client.query_api().query(query, org=org)[0].records:
+        data['timestamp'].append(record.values.get('_time'))
+        data[field].append(record.values.get('_value'))
+
+    return data;
+
+def updateDataPoints(df):
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    sequence = []
+
+    for index, row in df.iterrows():
+        anomaly = row['anomaly']
+        ticks = row['timestamp'].value
+        sequence.append(f'{measurement} anomaly={anomaly} {ticks}')
+
+    write_api.write(bucket, org, sequence)
 
 def anomaly(df):
     outliers_fraction = 0.06
-    X1 = df['A2_RSSI'].values.reshape(-1,1)
+    X1 = df[field].values.reshape(-1,1)
     model = IsolationForest(contamination=outliers_fraction)
     model.fit(X1)
     df['anomaly'] = pd.Series(model.predict(X1))
     return df
-#function returns the dataframe with anomaly column(-1 for anomalies)
+
+
+def main():    
+    data = getDataPoints(1000)
+    df = pd.DataFrame.from_dict(data)
+    result = anomaly(df)
+    updateDataPoints(result)
+
+main()
